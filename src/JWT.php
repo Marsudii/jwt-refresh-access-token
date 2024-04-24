09eed1fc4673d20c36b8d3fe5e0e85ca9d385cc9
Marsudi <marsudi124@gmail.com>
2024-04-24 13:45:00 +0700

    update migration dan code JWT

--- src/JWT.php
+++ src/JWT.php
@@ -6,32 +6,47 @@ use Firebase\JWT\JWT as FirebaseJWT;
 
 class JWT
 {
-    public function __construct()
+
+    public static function login(array $payload, $accessTokenEnv, $refreshTokenEnv, $algorithm)
     {
-
-    }
-
-    public function loginJWT(array $payload, $accessTokenEnv, $refreshTokenEnv, $algorithm)
-    {
-
         $accessToken = FirebaseJWT::encode($payload, $accessTokenEnv, $algorithm);
         $refreshToken = FirebaseJWT::encode($payload, $refreshTokenEnv, $algorithm);
-
         return [
             "access_token" => $accessToken,
             "refresh_token" => $refreshToken
         ];
+    }
+
+    public static function register(array $data)
+    {
+        return $data;
 
     }
 
-    public function refreshJWT()
+    public static function refresh(string $payload)
+    {
+        return $payload;
+    }
+
+    public static function logout(string $payload)
     {
 
     }
 
-    public function logoutJWT()
+    public static function checkExpired(string $payload)
     {
+        return $payload;
 
     }
 
+
+    public static function endcode(string $payload)
+    {
+        return $payload;
+
+    }
+    public static function decode()
+    {
+
+    }
 }
